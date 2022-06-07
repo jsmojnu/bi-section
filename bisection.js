f29535078be0008ed6f0a0a631b920b086652407
MD. MOJNU MIAH <94952925+jsmojnu@users.noreply.github.com>
2022-06-07 18:27:46 +0600

    bi--section

--- bisection.js
+++ bisection.js
@@ -1,34 +1,35 @@
 document.getElementById('calculate').addEventListener('click', function(){
     const a = +document.getElementById('a').value;
     const b = +document.getElementById('b').value;
+    const x4 = +document.getElementById('x4').value;
     const x3 = +document.getElementById('x3').value;
     const x2 = +document.getElementById('x2').value;
     const x1 = +document.getElementById('x1').value;
     const xMukto = +document.getElementById('xMukto').value;
-    console.log(bisection(a, b, x3, x2, x1, xMukto));
+    console.log(bisection(a, b, x4, x3, x2, x1, xMukto));
 })
 
 
 const epsilon = 0.0005;
 let c = 0;
 
-function equation(x , x3 , x2 , x1 , xMukto){
+function equation(x , x4, x3 , x2 , x1 , xMukto){
   
-    return x3 * x * x * x + x2 * x * x + x1 * x + xMukto;
+    return x4*x*x*x*x + x3 * x * x * x + x2 * x * x + x1 * x + xMukto;
 
 }
-function bisection(a, b, x3, x2, x1, xMukto) {
-    if (equation(a, x3, x2, x1, xMukto) * equation(b, x3, x2, x1, xMukto) >= 0) {
+function bisection(a, b, x4, x3, x2, x1, xMukto) {
+    if (equation(a, x4, x3, x2, x1, xMukto) * equation(b, x4, x3, x2, x1, xMukto) >= 0) {
         return "Sorry, change the value of a and b";
     }
     
    else{
     while ((b - a) >= epsilon) {
         c = (a + b)/2;
-        if (equation(c, x3, x2, x1, xMukto) === 0.0) {
+        if (equation(c, x4, x3, x2, x1, xMukto) === 0.0) {
             break;
         }
-        else if (equation(c, x3, x2, x1, xMukto) > 0) {
+        else if (equation(c, x4, x3, x2, x1, xMukto) > 0) {
             b = c;
         }
         else {
